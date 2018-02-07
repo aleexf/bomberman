bcdd704a4fcdbc048336104c339a51b701cc2c15
aleexf <aliaks35@gmail.com>
2018-02-07 17:23:02 +0300

    Added simple logger

---
+++ src/com/aleexf/logging/SimpleLogger.kt
@@ -0,0 +1,17 @@
+package com.aleexf.logging
+
+import java.util.Date
+import java.io.FileWriter
+import java.io.PrintWriter
+import java.text.SimpleDateFormat
+
+class Logger(val filename:String) {
+    private val formatter = SimpleDateFormat("[dd.MM.yyyy HH:mm:ss]")
+    private val oStream = PrintWriter(FileWriter(filename, true), true)
+    private fun getDate() = formatter.format(Date())
+    fun info(msg:String) = oStream.println("${getDate()}[INFO]: $msg")
+    fun warning(msg:String) = oStream.println("${getDate()}[WARNING]: $msg")
+    fun error(msg:String) = oStream.println("${getDate()}[ERROR]: $msg")
+}
+
+val logger = Logger(".\\logfile.log")
